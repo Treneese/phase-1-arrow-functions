2af2730019191a3f6cb4689357c2a93db50dcd5c
treneese <treneesejohnson9012@gmail.com>
2024-04-01 16:50:57 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,20 @@
+function divide(num1, num2){
+    //divides 2000 by 100
+//const num1 =2000
+//const num2 =100
+return num1 / num2
+}
+sum = divide(2000, 100)
+
+function square(num){
+    //multiplies it times itself
+    return num ** 2
+}
+square = squaredNum(6)
+
+function add(num1, num2){
+   // adds them together
+   return num1 + num2
+}
+
+sum = add(5, 400)
